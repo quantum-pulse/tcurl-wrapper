d5123c84eb98d6f0ef1a21546bfe76a150a95d67
Robert King <ro.king.eu@gmail.com>
2022-02-11 22:02:54 +0100

    editing of tests

--- src/tc-wpr/test/test_curl_mgr1.cxx
+++ src/tc-wpr/test/test_curl_mgr1.cxx
@@ -13,28 +13,5 @@ BOOST_AUTO_TEST_CASE(test_curl_mgr_status)
     curl_mgr l_mgr(url);
     auto l_status=l_mgr.status();
     BOOST_CHECK_EQUAL(l_status,0);
-    auto size_buffer=l_mgr.rbuffer().size();
-    BOOST_CHECK_EQUAL(size_buffer,45598);
 }
 
-/* usage of rbuffer method */
-BOOST_AUTO_TEST_CASE(test_curl_mgr_rbuffer1)
-{
-    string url="https://llvm.org/docs/CommandGuide/llvm-cov.html"; 
-    curl_mgr l_mgr(url);
-    auto l_status=l_mgr.status();
-    BOOST_CHECK_EQUAL(l_status,0);
-    auto size_buffer=l_mgr.rbuffer().size();
-    BOOST_CHECK_EQUAL(size_buffer,45598);
-}
-
-/* usage of functor */
-BOOST_AUTO_TEST_CASE(test_curl_mgr_rbuffer2)
-{
-    string url="https://llvm.org/docs/CommandGuide/llvm-cov.html"; 
-    curl_mgr l_mgr(url);
-    auto l_status=l_mgr.status();
-    BOOST_CHECK_EQUAL(l_status,0);
-    auto size_buffer=l_mgr().size();
-    BOOST_CHECK_EQUAL(size_buffer,45598);
-} 
